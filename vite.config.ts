ec9fc4fb98525c2b3964c6100f6c382df502c512
smoehrle <26185187+smoehrle@users.noreply.github.com>
2025-01-19 21:05:31 +0100

    Enable sourcemaps for dev

--- vite.config.ts
+++ vite.config.ts
@@ -2,9 +2,14 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 // https://vite.dev/config/
-export default defineConfig({
-  plugins: [react()],
-  optimizeDeps: {
-    exclude: ["js-big-decimal"],
-  },
+export default defineConfig(configEnv => {
+  return {
+    plugins: [react()],
+    optimizeDeps: {
+      exclude: ["js-big-decimal"],
+    },
+    build: {
+      sourcemap: configEnv.mode === 'development',
+    }
+  }
 });
