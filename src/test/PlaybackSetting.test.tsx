ef5b5075f5a6b343b2def13425327b2e7753a56f
Trigrab <trigrab@gmail.com>
2025-01-20 21:43:00 +0100

    add jest and first trivial test

---
+++ src/test/PlaybackSetting.test.tsx
@@ -0,0 +1,18 @@
+import {expect, test} from '@jest/globals';
+import {fireEvent, render} from '@testing-library/react'
+import PlaybackSetting from '../components/PlaybackSetting';
+
+
+
+test('PlaybackSetting changes playbackState', () => {
+  let playbackState = false;
+  const {queryByLabelText, getByLabelText} = render(
+    <PlaybackSetting label='TestSetting' isLoading={false} playbackSettingState={playbackState} setPlaybackSetting={(state) => {playbackState = state;}}/>
+  );
+
+  expect(queryByLabelText(/TestSetting/i)).toBeTruthy();
+
+  fireEvent.click(getByLabelText(/TestSetting/i));
+
+  expect(playbackState).toBeTruthy();
+});
